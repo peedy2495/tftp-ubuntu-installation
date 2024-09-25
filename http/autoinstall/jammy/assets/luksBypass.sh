f81a125cab66033f1a1a4876474cb119caf9b0f5
peedy2495 <mail.petermark@web.de>
2024-09-25 10:49:48 +0000

    changed: luks device token creation from manual setup to automatic determination.

--- http/autoinstall/jammy/assets/luksBypass.sh
+++ http/autoinstall/jammy/assets/luksBypass.sh
@@ -10,19 +10,23 @@
 exePath="$(dirname -- "${BASH_SOURCE[0]}")"
 
 passwd="$1"
-luksDevice="vda3"
 keyFilePath="/boot/keyfile"
 
-# create a random keyfile
+# Determine the encrypted Partition
+for d in /dev/*; do
+    sudo cryptsetup isLuks $d 2>/dev/null && luksDevice=$(echo $d | sed 's/\/dev\///')
+done
+
+# Create a random keyfile
 dd if=/dev/urandom of=/target$keyFilePath bs=1024 count=4
 chmod 0600 /target$keyFilePath
 
-# add the keyfile to luks
+# Add the keyfile to luks
 echo "ChangeMe" | cryptsetup luksAddKey --force-password /dev/$luksDevice /target$keyFilePath >>/target/root/debug.txt
 
-# preparing for using the kefile in initramfs
+# Preparing for using the kefile in initramfs
 echo "KEYFILE_PATTERN=$keyFilePath" >> /target/etc/cryptsetup-initramfs/conf-hook
 sed -i "s|none|${keyFilePath}|g" /target/etc/crypttab
 
-# bake nwe initramfs
+# Bake nwe initramfs
 curtin in-target --target=/target -- update-initramfs -u
